2d8ed15fd64656d1332bbfb41f5ed2e1c469c369
diego.moreira <diego.moreira@quiver.net.br>
2020-01-31 10:00:33 -0300

    Projeto inicializado e express instalado

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const targetBaseUrl = 'http://www.letsboot.com/';
+
+function handleRedirect(req, res) {
+  const targetUrl = targetBaseUrl + req.originalUrl;
+  res.redirect(targetUrl);
+}
+
+app.get('*', handleRedirect);
+
+const port = process.env.port || 3000;
+
+app.listen(port);
