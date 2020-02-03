dbcf6eeafe4ccc24244ac5bfa7a231c4db6d5c2f
diego.moreira <diego.moreira@quiver.net.br>
2020-02-03 09:29:25 -0300

    Utilização da lib express-http-proxy

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const express = require('express');
+const proxy = require('express-http-proxy');
 const app = express();
-const targetBaseUrl = 'http://www.letsboot.com/';
+const regex = /[w][w][w][\d]*[\d]/gm;
 
-function handleRedirect(req, res) {
-  const targetUrl = targetBaseUrl + req.originalUrl;
-  res.redirect(targetUrl);
+let m;
+let reqUrl;
+let targetUrl
+
+function selectProxyHost(req) {
+  reqUrl = req.host + req.url;
+  targetUrl = req.host + req.url;
+
+  while ((m = regex.exec(reqUrl)) !== null) {
+    if (m.index === regex.lastIndex) {
+        regex.lastIndex++;
+    }
+
+    m.forEach((match, groupIndex) => {
+      targetUrl = reqUrl.replace(match, "www");
+      req.headers.ambiente = match;
+    });
+  }
+
+  return targetUrl;
 }
 
-app.get('*', handleRedirect);
+app.use(proxy(selectProxyHost));
 
 const port = process.env.port || 3000;
 
-app.listen(port);
+app.listen(port, () => console.log("http_redirect inicializado"));
